677b667d2eb8828d5b4b9467f8b5ff5b361cb9d0
Shuler Littleton <shulerlittleton70@gmail.com>
2025-06-30 19:19:33 -0400

    Initial Requirements

--- tools.py
+++ tools.py
@@ -1,9 +1,9 @@
-from langchain_community.tools import WikipediaQueryRun, DuckDuckGoQueryRun #QuerryRuns
+from langchain_community.tools import WikipediaQueryRun, DuckDuckGoSearchRun #QuerryRuns
 from langchain_community.utilities import WikipediaAPIWrapper #APIWrapper
 from langchain.tools import Tool
 from datetime import datetime
 
-search = DuckDuckGoQueryRun()
+search = DuckDuckGoSearchRun()
 search_tool = Tool(
     name="Search",
     func=search.run,
