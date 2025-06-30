aff339cfac1a7eec7e9eaeff52ce8832e8264a72
Shuler Littleton <shulerlittleton70@gmail.com>
2025-06-30 19:40:27 -0400

    Initial Requirements

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from typing import List
 from dotenv import load_dotenv
 from pydantic import BaseModel
-from tools import search_tool
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain.agents import create_tool_calling_agent
 from langchain.agents import AgentExecutor
+from tools import search_tool,wiki_tool
+
 
 
 load_dotenv() #loads variables such as api keys from the .env text file.
@@ -18,7 +19,6 @@ class ResearchResponse(BaseModel):
     sources: List[str]
     tools_used: list[str]
 
-#establish potential llms and their model version.
 
 #llm = ChatOpenAI(model = "gpt-4o-mini")
 llm = ChatAnthropic(model = "claude-sonnet-4-20250514")
@@ -41,7 +41,7 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 ).partial(format_instructions=parser.get_format_instructions())
 
-tools =[search_tool]
+tools = [search_tool, wiki_tool]
 agent = create_tool_calling_agent(
     llm=llm,
     prompt=prompt,
--- tools.py
+++ tools.py
@@ -1,11 +1,22 @@
-from langchain_community.tools import WikipediaQueryRun, DuckDuckGoSearchRun #QuerryRuns
-from langchain_community.utilities import WikipediaAPIWrapper #APIWrapper
+from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_community.utilities import SerpAPIWrapper
+from langchain_community.utilities import WikipediaAPIWrapper
 from langchain.tools import Tool
-from datetime import datetime
+from dotenv import load_dotenv
 
-search = DuckDuckGoSearchRun()
+load_dotenv()
+
+search = SerpAPIWrapper()
 search_tool = Tool(
     name="Search",
     func=search.run,
-    description="Search the web for information",
+    description="Search the web using SerpAPI (Google Results)"
+)
+
+# Wikipedia tool (corrected)
+wiki_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=100)
+wiki_tool = Tool(
+    name="Wikipedia",
+    func=wiki_wrapper.run,
+    description="Search for factual information from Wikipedia",
 )
