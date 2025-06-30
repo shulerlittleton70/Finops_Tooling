8c5c447f37e230ebdebc121652142636f614e400
Shuler Littleton <shulerlittleton70@gmail.com>
2025-06-30 18:53:52 -0400

    Initial Requirements

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
+from typing import List
+
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
 
-load_dotenv()
+load_dotenv() #loads variables such as api keys from the .env text file.
 
-#establish potential llms and their model version. Would like to write documentation up on available models and which to select
-llm2 = ChatOpenAI(model = "gpt-4o-mini")
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: List[str]
+    tools_used: list[str]
+
+#establish potential llms and their model version.
+
+#llm = ChatOpenAI(model = "gpt-4o-mini")
 llm = ChatAnthropic(model = "claude-sonnet-4-20250514")
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
 
-response = llm.invoke("What is the meaning of life?")
-print(response.content)
-
-
-
-
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system", #this is the agent prompt
+            """
+            Your are a research assistant that will help generate a research paper. 
+            Answer the user query and use neccessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(fromat_instrucitons=parser.fet_format_instructions())
 
 
 
