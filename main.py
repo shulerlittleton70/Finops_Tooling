4c19ffd8b0387212c258a829dbde97e7d2d958cc
Shuler Littleton <shulerlittleton70@gmail.com>
2025-06-30 18:30:45 -0400

    Initial Requirements

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ from langchain_anthropic import ChatAnthropic
 load_dotenv()
 
 #establish potential llms and their model version. Would like to write documentation up on available models and which to select
-llm = ChatOpenAI(model = "gpt-4o-mini")
-llm2 = ChatAnthropic(model = "claud-3-5-sonnet-20241022")
+llm2 = ChatOpenAI(model = "gpt-4o-mini")
+llm = ChatAnthropic(model = "claud-3-5-sonnet-20241022")
+
+response = llm.invoke("What is the meaning of life?")
+print(response)
 
 
 
