586755d94f0b10df4ac4a5eec4522bd200052057
Shuler Littleton <shulerlittleton70@gmail.com>
2025-06-30 19:48:11 -0400

    Initial Requirements

--- tools.py
+++ tools.py
@@ -3,8 +3,17 @@ from langchain_community.utilities import SerpAPIWrapper
 from langchain_community.utilities import WikipediaAPIWrapper
 from langchain.tools import Tool
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
+
+def save_to_txt(data:str, filename: str ="research_output.txt"):
+    timestamp = datetime.now().strftime("%m-%d-%Y-%H-%M-%S")
+    formatted_text = f'---Research Output ---\nTimestamp: {timestamp}\n\n{data}\n\n'
+    with open(filename,"a", encoding= "utf-8") as f:
+        f.write(formatted_text)
+
+    return f"Data  successfully saved to {filename}"
 
 search = SerpAPIWrapper()
 search_tool = Tool(
